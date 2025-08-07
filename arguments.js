6f401bf8576ff0f55f2793019c3e365c1be9e4a3
provash10 <provash20cb@gmail.com>
2025-08-08 00:57:29 +0600

    19-4 How Function works and argument vs parameters

---
+++ arguments.js
@@ -0,0 +1,31 @@
+function doubleIt(number){
+    const doubled = number * 2;
+    console.log(number, doubled);
+}
+console.log('I will call the function');
+doubleIt(15);
+console.log('---------------------');
+doubleIt(85);
+console.log('---------------------');
+doubleIt(115);
+console.log('---------------------');
+
+const number = 55; 
+doubleIt(number);
+const money= 112; 
+doubleIt(money);
+
+//
+
+function difference(num1, num2){
+    const diff = num1-num2;
+    console.log(num1, num2, 'difference is ', diff);
+
+}
+
+const fatherAge = 40;
+const myAge = 10;
+
+difference(fatherAge, myAge);
+
+
