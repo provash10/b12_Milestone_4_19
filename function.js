d8cc7e0fcdb5b600dcf2d256e9773f29c6d61fc8
provash10 <provash20cb@gmail.com>
2025-08-07 21:20:20 +0600

    19-1 What are function and function syntax

---
+++ function.js
@@ -0,0 +1,28 @@
+// function structure
+// function name(){
+
+// }
+
+const number = 45;
+
+// function declare
+function fanOff(){
+    console.log('Stand Up');
+    console.log('Walk  forward');
+    console.log('Click The Switch');
+}
+
+// call the function
+fanOff();
+
+const price = 45;
+
+// function declare
+function brushTeeth(){
+    console.log('Take Brush');
+    console.log('Add Paste');
+    console.log('Doning Brush');
+}
+
+// call the function
+brushTeeth();
